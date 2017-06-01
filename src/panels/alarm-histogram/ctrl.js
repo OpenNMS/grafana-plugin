0bebd80f1ea8fbee751eb47544a679faaabfd2c1
Dustin Frisch <fooker@lab.sh>
2017-06-01 15:48:28 +0200

    Colors

--- src/panels/alarm-histogram/ctrl.js
+++ src/panels/alarm-histogram/ctrl.js
@@ -58,37 +58,37 @@ class AlarmHistogramCtrl extends MetricsPanelCtrl {
                 this.series = [
                     {
                         name: 'Cleared',
-                        count: _.defaultTo(counts['CLEARED'], 10),
+                        count: _.defaultTo(counts['CLEARED'], 0),
                         color: this.scope.$root.colors[23],
                     },
                     {
                         name: 'Normal',
-                        count: _.defaultTo(counts['NORMAL'], 10),
+                        count: _.defaultTo(counts['NORMAL'], 0),
                         color: this.scope.$root.colors[0],
                     },
                     {
                         name: 'Indeterm.',
-                        count: _.defaultTo(counts['INDETERMINATE'], 10),
+                        count: _.defaultTo(counts['INDETERMINATE'], 0),
                         color: this.scope.$root.colors[55],
                     },
                     {
                         name: 'Warning',
-                        count: _.defaultTo(counts['WARNING'], 10),
+                        count: _.defaultTo(counts['WARNING'], 0),
                         color: this.scope.$root.colors[1],
                     },
                     {
                         name: 'Minor',
-                        count: _.defaultTo(counts['MINOR'], 10),
+                        count: _.defaultTo(counts['MINOR'], 0),
                         color: this.scope.$root.colors[3],
                     },
                     {
                         name: 'Major',
-                        count: _.defaultTo(counts['MAJOR'], 10),
+                        count: _.defaultTo(counts['MAJOR'], 0),
                         color: this.scope.$root.colors[4],
                     },
                     {
                         name: 'Critical',
-                        count: _.defaultTo(counts['CRITICAL'], 10),
+                        count: _.defaultTo(counts['CRITICAL'], 0),
                         color: this.scope.$root.colors[28],
                     },
 
