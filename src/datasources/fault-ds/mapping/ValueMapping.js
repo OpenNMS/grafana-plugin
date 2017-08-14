12c6a28232876a82bad43eec892c249cbe472fb2
Markus von Rüden <mvr@opennms.com>
2017-08-14 09:52:21 +0200

    HELM-35: Defer null value handling to opennms.js client

--- src/datasources/fault-ds/mapping/ValueMapping.js
+++ src/datasources/fault-ds/mapping/ValueMapping.js
@@ -1,34 +1,10 @@
 
 export class ValueMapping {
     getApiValue(internalAttribute, value) {
-        // TODO MVR value mapping should be moved to the opennms-js implementation
-        // if (internalAttribute === 'alarmAckTime') {
-        //     if (value === 'null') {
-        //         value = new Date(0);
-        //     }
-        //
-        //     // yyyy-MM-ddTHH:mm:ss.sssZ
-        //     // 2017-06-08T10:17:17.173+0200
-        //     value = value.toJSON().replace("Z", "+0000"); // make it parsable by java
-        // }
-        if ("null" === value) {
-            value = '\u0000';
-        }
         return value;
     }
 
     getUiValue(internalAttribute, value) {
-        // TODO MVR value mapping should be moved to the opennms-js implementation
-        // if (internalAttribute === 'alarmAckTime') {
-        //     if (value === new Date(0)) {
-        //         return 'null';
-        //     }
-        //     var stringified = JSON.parse(value.replace("+0000", "Z"));
-        //     return new Date(stringified);
-        // }
-        if (value === "\u0000") {
-            return "null";
-        }
         return value;
     }
 }
