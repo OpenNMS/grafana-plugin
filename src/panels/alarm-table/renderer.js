40bff023f6772913f31e872812b4c49a14a3f868
Markus von Rüden <mvr@opennms.com>
2017-08-17 16:03:12 +0200

    HELM-15: Make options visible depending on alarm state

--- src/panels/alarm-table/renderer.js
+++ src/panels/alarm-table/renderer.js
@@ -250,10 +250,11 @@ export class TableRenderer {
                 new Group().withItem(new MenuItem("Details", `ctrl.alarmDetails('${source}', ${alarm.id})`)))
             .withGroup(
                 new Group()
-                    .withItem(new MenuItem("Acknowledge", `ctrl.acknowledgeAlarm('${source}', ${alarm.id})`))
-                    .withItem(new MenuItem("Unacknowledge", `ctrl.unacknowledgeAlarm('${source}', ${alarm.id})`))
-                    .withItem(new MenuItem("Clear", `ctrl.clearAlarm('${source}', ${alarm.id})`))
-                    .withItem(new MenuItem("Escalate", `ctrl.escalateAlarm('${source}', ${alarm.id})`)))
+                    .withItem(new MenuItem("Acknowledge", `ctrl.acknowledgeAlarm('${source}', ${alarm.id})`, () => alarm.ackTime === void 0))
+                    .withItem(new MenuItem("Unacknowledge", `ctrl.unacknowledgeAlarm('${source}', ${alarm.id})`, () => alarm.ackTime))
+                    .withItem(new MenuItem("Clear", `ctrl.clearAlarm('${source}', ${alarm.id})`, () => alarm.severity.index >= Model.Severities.NORMAL.index && alarm.severity.index <= Model.Severities.CRITICAL.index))
+                    .withItem(new MenuItem("Escalate", `ctrl.escalateAlarm('${source}', ${alarm.id})`, () => alarm.severity.index == Model.Severities.CLEARED.index || alarm.severity.index >= Model.Severities.NORMAL.index && alarm.severity.index < Model.Severities.CRITICAL.index))
+            )
             .withGroup(
                 new Group()
                     .withVisibility(() => ticketerConfig && ticketerConfig.enabled)
