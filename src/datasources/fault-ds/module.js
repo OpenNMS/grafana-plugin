b662d3e3928e336ea850e4fe29ec1dbf6358bca4
Markus von Rüden <mvr@opennms.com>
2017-08-23 14:22:32 +0200

    HELM-43: Provide examples and allow users to create new query from any example

--- src/datasources/fault-ds/module.js
+++ src/datasources/fault-ds/module.js
@@ -1,11 +1,25 @@
 import {OpenNMSFMDatasource} from './datasource';
 import {OpenNMSFMDatasourceQueryCtrl} from './query_ctrl';
-
+import {Examples} from './Examples';
 
 class OpenNMSFMDatasourceConfigCtrl {}
 OpenNMSFMDatasourceConfigCtrl.templateUrl = 'datasources/fault-ds/partials/config.html';
 
-class OpenNMSFMDatasourceQueryOptionsCtrl {}
+class OpenNMSFMDatasourceQueryOptionsCtrl {
+  constructor(uiSegmentSrv) {
+    this.uiSegmentSrv = uiSegmentSrv;
+    this.examples = Examples;
+  }
+
+  createQueryFromExample(example) {
+      const target = {
+        isNew: true,
+        filter: example.apiFilter
+      };
+
+      this.panelCtrl.panel.targets.push(target);
+  }
+}
 OpenNMSFMDatasourceQueryOptionsCtrl.templateUrl = 'datasources/fault-ds/partials/query.options.html';
 
 export {
