c835abc9002aba16386eaf6cac49f6aa021d6056
Benjamin Reed <ranger@opennms.org>
2018-10-16 11:45:53 -0400

    HELM-73: make $node template variable work in alarm queries

--- src/datasources/fault-ds/datasource.js
+++ src/datasources/fault-ds/datasource.js
@@ -74,16 +74,29 @@ export class OpenNMSFMDatasource {
       const self = this;
       _.each(clauses, clause => {
         if (clause.restriction) {
-            if (clause.restriction instanceof API.NestedRestriction) {
-                self.substitute(clause.restriction.clauses, options);
-            } else if (clause.restriction.value) {
+            const restriction = clause.restriction;
+            if (restriction instanceof API.NestedRestriction) {
+                self.substitute(restriction.clauses, options);
+            } else if (restriction.value) {
                 // Range must be of type date, otherwise it is not parseable by the OpenNMS client
-                if (clause.restriction.value === '$range_from' || clause.restriction.value === "[[range_from]]") {
-                    clause.restriction.value = options.range.from;
-                } else if (clause.restriction.value === '$range_to' || clause.restriction.value === "[[range_to]]") {
-                    clause.restriction.value = options.range.to;
+                if (restriction.value === '$range_from' || restriction.value === "[[range_from]]") {
+                    restriction.value = options.range.from;
+                } else if (restriction.value === '$range_to' || restriction.value === "[[range_to]]") {
+                    restriction.value = options.range.to;
                 } else {
-                    clause.restriction.value = self.templateSrv.replace(clause.restriction.value, options.scopedVars);
+                    restriction.value = self.templateSrv.replace(restriction.value, options.scopedVars);
+                }
+                // Turn node criteria fs:fid into separate clauses
+                if (restriction.attribute === 'node' && restriction.value.indexOf(':') > 0) {
+                    if (restriction.comparator.id !== API.Comparators.EQ.id) {
+                        console.log('WARNING: Using a comparator other than EQ will probably not work as expected with a foreignSource:foreignId node criteria.');
+                    }
+                    const nodeCriteria = restriction.value.split(':');
+                    const replacement = new API.NestedRestriction(
+                        new API.Clause(new API.Restriction('node.foreignSource', restriction.comparator, nodeCriteria[0]), API.Operators.AND),
+                        new API.Clause(new API.Restriction('node.foreignId', restriction.comparator, nodeCriteria[1]), API.Operators.AND),
+                    );
+                    clause.restriction = replacement;
                 }
             }
         }
