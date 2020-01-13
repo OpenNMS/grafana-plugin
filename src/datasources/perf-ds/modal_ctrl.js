2a209efb6c6ba8bc75bcb84ad42542c4b4fe7463
QauseenMZ <qmaab07@gmail.com>
2020-01-13 16:31:41 -0500

    HELM-64: Pagination Added to Modal Controller
    * HELM - 64: Pagination Added
    
    * HELM-64: Modal Footer CSS
    
    * HELM-64: Replaced Fixed Value with pageSize, Next Button Disable Condition Updated
    
    * HELM-64: Client-Size Pagination for Results > PageSize
    
    * HELM-64: Unused Count Params Removed
    
    * HELM-64: Result Count Label Updated

--- src/datasources/perf-ds/modal_ctrl.js
+++ src/datasources/perf-ds/modal_ctrl.js
@@ -3,27 +3,54 @@ class ModalCtrl {
   constructor($scope) {
     this.$scope = $scope;
     this.query = "";
+    this.offset = 0;
     this.selectedRow = null;
-
+    this.currentPage = 0;
+    this.pageSize = 25;
+    this.selfPagination = false; // Flag for client-side pagination
     this.searchForRows();
   }
 
   searchForRows() {
     var self = this;
     this.searching = true;
-    this.$scope.search(this.query)
+    
+    this.$scope.search(this.query, this.offset)
       .then(function (results) {
         // Reset the selected row
         self.selectedRow = null;
+
         // Add the results to the scope
-        self.rows = results.rows;
-        self.count = results.count;
+        self.allResult = results.rows;
+
+        // Apply client-side pagination if result is > pageSize
+        if(results.rows.length > self.pageSize){
+          self.selfPagination = true;
+          self.count = 0;
+          self.updateSelfPageinatedData();
+        }
+        else {
+          self.rows = results.rows;  // When paginated or limited result received
+          self.count = (results.count ? results.count : self.rows.length) + self.offset;
+        }
+
         self.totalCount = results.totalCount;
+        self.numberOfPages= Math.ceil(self.totalCount/self.pageSize);                
+
         // We're done
         self.searching = false;
       }, function () {
         self.searching = false;
       });
+  }
+
+  updateSelfPageinatedData(){
+    this.rows =  this.allResult.slice(this.offset, (this.offset + this.pageSize));
+    this.count += this.rows.length;
+  }
+
+  startFrom(currentPage, pageSize) {
+    return currentPage*pageSize;
   }
 
   setClickedRow(index) {
@@ -34,6 +61,31 @@ class ModalCtrl {
       // Keep a reference to the row when the selection is made
       this.row = this.rows[this.selectedRow];
     }
+  }
+
+  nextPage(){
+    if((this.currentPage + 1) == this.$scope.ctrl.numberOfPages){
+      return; // Maximum pages reached
+    }
+
+    this.offset += this.pageSize;
+    this.currentPage++;
+    
+    if(this.selfPagination){
+      this.updateSelfPageinatedData();
+    }
+    else{
+      this.searchForRows();
+    }
+  }
+
+  prevPage(){
+    if(this.currentPage == 0){
+      return;
+    }
+    this.offset -= this.pageSize;
+    this.currentPage--;
+    this.searchForRows();
   }
 
   cancel() {
