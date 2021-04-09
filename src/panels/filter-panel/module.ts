6cf0b203869aaa70a22749487b16261a2d8eda88
Benjamin Reed <ranger@opennms.org>
2021-04-09 17:21:38 -0400

    fix compiling of CSS

--- src/panels/filter-panel/module.ts
+++ src/panels/filter-panel/module.ts
@@ -1,11 +1,5 @@
 import { FilterCtrl } from './ctrl';
-import { loadPluginCss } from '@grafana/runtime';
 
 import './value_select_dropdown';
-
-loadPluginCss({
-  dark: 'plugins/opennms-helm-app/panels/filter-panel/css/filter.dark.css',
-  light: 'plugins/opennms-helm-app/panels/filter-panel/css/filter.light.css',
-});
 
 export { FilterCtrl as PanelCtrl };
--- src/panels/flow-histogram/module.ts
+++ src/panels/flow-histogram/module.ts
@@ -1,9 +1,3 @@
 import { HelmHistogramCtrl } from './ctrl';
-import { loadPluginCss } from '@grafana/runtime';
-
-loadPluginCss({
-  dark: 'plugins/opennms-helm-app/panels/flow-histogram/css/legend.dark.css',
-  light: 'plugins/opennms-helm-app/panels/flow-histogram/css/legend.light.css',
-});
 
 export { HelmHistogramCtrl, HelmHistogramCtrl as PanelCtrl };
