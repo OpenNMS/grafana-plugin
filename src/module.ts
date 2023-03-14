384b7307b3dad91f21ac87b0c100163cebcae3fd
synqotik <stheleman@opennms.com>
2023-03-14 10:26:46 -0400

    HELM-376: Add React App component

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,17 @@
-import { OpenNMSHelmAppConfigCtrl } from './components/config';
+//import { OpenNMSHelmAppConfigCtrl } from './components/config';
+import { AppPlugin } from '@grafana/data'
+import { App } from './components/App'
+import { AppConfig } from './components/AppConfig'
 
-import { initializeCss } from 'lib/utils';
+//import { initializeCss } from 'lib/utils'
 
-initializeCss();
+//initializeCss();
 
-export { OpenNMSHelmAppConfigCtrl as ConfigCtrl };
+//export { OpenNMSHelmAppConfigCtrl as ConfigCtrl };
+
+export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
+  title: 'Configuration',
+  icon: 'cog',
+  body: AppConfig,
+  id: 'configuration'
+})
