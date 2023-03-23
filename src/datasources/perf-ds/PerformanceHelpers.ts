b38c20576e02e11976778072899fc3c66c8498d1
aramosvizcarra <aramos-vizcarra@nanthealth.com>
2023-03-23 14:28:34 -0400

    Fix: refactor a bit the code to create one dataFrame per query in an editor. (#644)
    * refactor a bit the code to create one dataFrame per query in an editor.
    
    * changed the loginc to avoid processing dataframes when there is no data and return just empty dataframes

--- src/datasources/perf-ds/PerformanceHelpers.ts
+++ src/datasources/perf-ds/PerformanceHelpers.ts
@@ -9,93 +9,104 @@ import { OnmsMeasurementsQueryResponse } from "./types";
  * @param end an ending timestamp for the window, or 0 to not limit the end of the window range
  */
 const getWindowedTimestamps = (timestamps: number[], start: number, end: number) => {
-  if (start === 0 && end === 0) {
-    return {
-      windowedTimestamps: [...timestamps],
-      startIndex: 0,
-      endIndex: timestamps.length - 1
+    if (start === 0 && end === 0) {
+        return {
+            windowedTimestamps: [...timestamps],
+            startIndex: 0,
+            endIndex: timestamps.length - 1
+        }
     }
-  }
 
-  const windowedTimestamps = [] as number[]
-  let startIndex = -1
-  let endIndex = timestamps.length - 1
+    const windowedTimestamps = [] as number[]
+    let startIndex = -1
+    let endIndex = timestamps.length - 1
 
-  for (let i = 0; i < timestamps.length; i++) {
-      const tsVal = timestamps[i]
+    for (let i = 0; i < timestamps.length; i++) {
+        const tsVal = timestamps[i]
 
-      if (end > 0 && tsVal > end) {
-          endIndex = i - 1
-          break
-      }
+        if (end > 0 && tsVal > end) {
+            endIndex = i - 1
+            break
+        }
 
-      if (start === 0 || tsVal >= start) {
-          if (startIndex < 0) {
-              startIndex = i
-          }
-          windowedTimestamps.push(tsVal)
-      }
-  }
+        if (start === 0 || tsVal >= start) {
+            if (startIndex < 0) {
+                startIndex = i
+            }
+            windowedTimestamps.push(tsVal)
+        }
+    }
 
-  return {
-    windowedTimestamps,
-    startIndex,
-    endIndex
-  }
+    return {
+        windowedTimestamps,
+        startIndex,
+        endIndex
+    }
 }
 
 /**
  * Convert QueryResponse data returned by OpenNMS Measurements Rest API to Grafana DataFrame format.
  */
 export const measurementResponseToDataFrame =
-    (measurementData: OnmsMeasurementsQueryResponse, queryRefId?: string): DataFrame => {
+    (measurementData: OnmsMeasurementsQueryResponse): DataFrame[] => {
 
-    const { start, end, labels, columns, timestamps, metadata } = measurementData
+        const { start, end, labels, columns, timestamps, metadata } = measurementData
 
-    const dataFrame: DataFrame = {
+        const dataFrames: DataFrame[] = []
+
+        let dataFrame: DataFrame = getEmptyDataFrame()
+
+        if (!timestamps || !timestamps.length) {            
+            dataFrames.push(dataFrame)
+        } else {
+            const { windowedTimestamps, startIndex, endIndex } = getWindowedTimestamps(timestamps, start, end)
+
+            // no data or no data within the start/end timespan, return an empty DataFrame
+            if (windowedTimestamps.length === 0) {
+                dataFrames.push(dataFrame)
+            } else {
+
+                for (let i = 0; i < labels?.length; i++) {
+                    dataFrame = getEmptyDataFrame()
+
+                    dataFrame.length = windowedTimestamps.length
+
+                    dataFrame.fields.push({
+                        name: 'Time',
+                        type: FieldType.time,
+                        config: {},
+                        values: new ArrayVector<number>(windowedTimestamps)
+                    } as Field)
+
+                    const label = metadata && metadata.resources ?
+                        FunctionFormatter.format(labels[i], metadata) :
+                        labels[i]
+
+                    if (columns && columns.length) {
+
+                        const column = columns[i]
+                        const windowedValues = column.values.slice(startIndex, endIndex + 1)
+
+                        let field = {
+                            name: label || 'Value',
+                            type: FieldType.number, // number but actual data may be a string representing a number or "NaN"
+                            config: {},
+                            values: new ArrayVector<string | number | null>(windowedValues)
+                        } as Field
+                        dataFrame.fields.push(field)
+                    }
+                    dataFrames.push(dataFrame)
+                }
+            }
+        }
+
+        return dataFrames
+    }
+
+const getEmptyDataFrame = () => {
+    return {
         name: '',
-        refId: queryRefId,
         length: 0,
         fields: []
     }
-
-    if (!columns || !columns.length || !timestamps || !timestamps.length) {
-        return dataFrame
-    }
-
-    const { windowedTimestamps, startIndex, endIndex } = getWindowedTimestamps(timestamps, start, end)
-
-    // no data or no data within the start/end timespan, return an empty DataFrame
-    if (windowedTimestamps.length === 0) {
-        return dataFrame
-    }
-
-    dataFrame.length = windowedTimestamps.length
-
-    dataFrame.fields.push({
-        name: 'Time',
-        type: FieldType.time,
-        config: {},
-        values: new ArrayVector<number>(windowedTimestamps)
-    } as Field)
-
-    for (let i = 0; i < columns?.length; i++) {
-        let label = metadata && metadata.resources ?
-            FunctionFormatter.format(labels[i], metadata) :
-            labels[i];
-
-        const column = columns[i]
-        const windowedValues = column.values.slice(startIndex, endIndex + 1)
-
-        let field = {
-            name: label || 'Value',
-            type: FieldType.number, // number but actual data may be a string representing a number or "NaN"
-            config: {},
-            values: new ArrayVector<string | number | null>(windowedValues)
-        } as Field
-
-        dataFrame.fields.push(field)
-    }
-
-    return dataFrame
 }
