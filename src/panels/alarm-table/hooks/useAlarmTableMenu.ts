5562bc0f6677c6b7eaab32a0e983c7af5c30d407
synqotik <stheleman@opennms.com>
2023-04-14 10:56:37 -0400

    HELM-403: Ensure Alarm Table click/contextMenu work when panel data is updated

--- src/panels/alarm-table/hooks/useAlarmTableMenu.ts
+++ src/panels/alarm-table/hooks/useAlarmTableMenu.ts
@@ -1,9 +1,11 @@
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-export const useAlarmTableMenu = (rowClicked) => {
+export const useAlarmTableMenu = (rowClicked, series) => {
 
     const [menu, setMenu] = useState({ x: 0, y: 0 })
     const [menuOpen, setMenuOpen] = useState(false)
+
+    const table = useRef<HTMLDivElement>(null);
 
     const contextMenu = (index: number, e: MouseEvent) => {
         e.preventDefault()
@@ -12,53 +14,65 @@ export const useAlarmTableMenu = (rowClicked) => {
         setMenuOpen(() => true)
     }
 
-    const table = useRef<HTMLDivElement>(null);
-
     const addClassToTableBody = () => {
         const headerGroup = table.current?.querySelector('div[role="rowgroup"] + div')
         headerGroup?.classList.add('table-body')
     }
 
-    const processRowClicked = (e: Event) => {
-        const index = (e.currentTarget as Element).getAttribute('data-rowindex')
+    // user click is in a table cell, we find its parent row's index to
+    // know which alarm item was clicked
+    const getIndexFromCell = (cell: HTMLElement) => {
+      const row = cell.parentElement
+      const rows = table.current?.querySelectorAll('.table-body div[role="row"]')
 
-        if (index !== null && index?.length > 0) {
-          rowClicked(parseInt(index, 10), e as MouseEvent, false)
+      let rowIndex = -1
+
+      if (rows && rows.length) {
+        for (let i = 0; i < rows.length; i++) {
+          // reference equality
+          if (rows[i] === row) {
+            rowIndex = i
+            break
+          }
         }
+      }
+
+      return rowIndex
     }
 
-    const processContextMenu = (e: Event) => {
-        const index = (e.currentTarget as Element).getAttribute('data-rowindex')
+    const onTableClicked = (e: Event) => {
+      const rowIndex = getIndexFromCell(e.target as HTMLElement)
 
-        if (index !== null && index?.length > 0) {
-          contextMenu(parseInt(index, 10), e as MouseEvent)
-        }
+      if (rowIndex >= 0) {
+        rowClicked(rowIndex, e as MouseEvent, false)
+      }
+    }
+
+    const onTableContextMenu = (e: Event) => {
+      const rowIndex = getIndexFromCell(e.target as HTMLElement)
+
+      if (rowIndex >= 0) {
+        contextMenu(rowIndex, e as MouseEvent)
+      }
     }
 
     useEffect(() => {
-        addClassToTableBody()
+        const currentTable = table.current
 
-        const rows = table.current?.querySelectorAll('.table-body div[role="row"]')
-
-        const onRowClicked = (e: Event) => processRowClicked(e)
-        const onContextMenu = (e: Event) => processContextMenu(e)
-
-        rows?.forEach((row, index) => {
-            row.setAttribute('data-rowindex', '' + index)
-            row.addEventListener('click', onRowClicked)
-            row.addEventListener('contextmenu', onContextMenu)
-        })
+        table.current?.addEventListener('click', onTableClicked)
+        table.current?.addEventListener('contextmenu', onTableContextMenu)
 
         return () => {
-          if (rows && rows.length > 0) {
-            rows.forEach((row) => {
-              row.removeEventListener('click', onRowClicked)
-              row.removeEventListener('contextmenu', onContextMenu)
-            })
-          }
+          currentTable?.removeEventListener('click', onTableClicked)
+          currentTable?.removeEventListener('contextmenu', onTableContextMenu)
         }
+
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [table, menu, menuOpen, rowClicked])
+    }, [table.current])
+
+    useEffect(() => {
+        addClassToTableBody()
+    }, [menu, menuOpen, rowClicked, series])
 
     return { table, menu, menuOpen, setMenu, setMenuOpen }
 }
