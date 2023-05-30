e0015302238b1540e709cac208c116aff7fff56c
synqotik <stheleman@opennms.com>
2023-05-30 17:54:39 -0400

    HELM-422: Flow Dashboard and Graph Panel conversion and query fixes

--- src/lib/dashboard-convert/flowDs.ts
+++ src/lib/dashboard-convert/flowDs.ts
@@ -1,12 +1,56 @@
+import { FlowSegments, FlowSegmentStringsLowercase } from '../../datasources/flow-ds/constants'
+
 export const updateFlowQuery = (source: any) => {
+  // items that are the same:
+  // N
+  // interfaceId
+  // nodeCriteria
+
   let target: any = {
     ...source,
+    segment: convertSegment(source),
     functions: convertFunctions(source),
     functionParameters: convertFunctionParameters(source),
     parameterOptions: convertParameterOptions(source)
   }
 
+  // metric is replaced by segment
+  delete target.metric
+
   return target
+}
+
+const convertSegment = (target: any) => {
+  if (target.metric) {
+    const metric = target.metric.toLowerCase()
+
+    if (metric === FlowSegmentStringsLowercase.Applications) {
+      return {
+        value: FlowSegments.Applications,
+        label: FlowSegmentStringsLowercase.Applications
+      }
+    } else if (metric === FlowSegmentStringsLowercase.Conversations) {
+      return {
+        value: FlowSegments.Conversations,
+        label: FlowSegmentStringsLowercase.Conversations
+      }
+    } else if (metric === FlowSegmentStringsLowercase.Dscps) {
+      return {
+        value: FlowSegments.Dscps,
+        label: FlowSegmentStringsLowercase.Dscps
+      }
+    } else if (metric === FlowSegmentStringsLowercase.Hosts) {
+      return {
+        value: FlowSegments.Hosts,
+        label: FlowSegmentStringsLowercase.Hosts
+      }
+    }
+  }
+
+  return {
+    value: -1,
+    label: ''
+  }
 }
 
 const convertFunctions = (target: any) => {
