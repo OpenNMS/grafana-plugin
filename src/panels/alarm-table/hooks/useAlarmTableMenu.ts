1f4d0bd385780a59f7f25c6fe96953826f5d16f5
Scott Theleman <stheleman@opennms.com>
2024-05-07 15:09:32 -0400

    OPG-480: Alarm Table Panel scrolling issue fix

--- src/panels/alarm-table/hooks/useAlarmTableMenu.ts
+++ src/panels/alarm-table/hooks/useAlarmTableMenu.ts
@@ -1,87 +1,93 @@
-import { MutableRefObject, useEffect, useRef, useState } from 'react'
+import { MutableRefObject, RefObject, useEffect, useState } from 'react'
+import { DataFrame } from '@grafana/data'
+import { getAlarmIdFromCell } from '../AlarmTableHelper'
 
-export const useAlarmTableMenu = (indexes: MutableRefObject<boolean[]>, rowClicked, series, setState) => {
-    const [menu, setMenu] = useState({ x: 0, y: 0 })
-    const [menuOpen, setMenuOpen] = useState(false)
+/**
+ * 
+ * @param indexes Row indexes and whether they are selected or not
+ * @param selectedAlarmIds Set of selected alarm ids
+ * @param rowClicked Row clicked handler
+ * @param series Ref to the DataFrame of the alarm data
+ * @param setAlarmControlState Set the AlarmTableControlState
+ * @returns 
+ */
+export const useAlarmTableMenu = (
+  table: RefObject<HTMLDivElement>,
+  indexes: MutableRefObject<boolean[]>,
+  selectedAlarmIds: MutableRefObject<Set<number>>,
+  rowClicked,
+  series: MutableRefObject<DataFrame>,
+  setAlarmControlState
+) => {
 
-    const table = useRef<HTMLDivElement>(null)
+  const [menu, setMenu] = useState({ x: 0, y: 0 })
+  const [menuOpen, setMenuOpen] = useState(false)
 
-    const contextMenu = (index: number, e: MouseEvent) => {
-        e.preventDefault()
-        if (index >= 0) {
-            rowClicked(index, e, true)
-        }
-        setMenu({ x: e.x, y: e.y })
-        setMenuOpen(() => true)
-    }
+  const contextMenu = (alarmId: number, e: MouseEvent) => {
+    e.preventDefault()
 
-    const addClassToTableBody = () => {
-        const headerGroup = table.current?.querySelector('div[role="rowgroup"] + div')
-        headerGroup?.classList.add('table-body')
-    }
-
-    // user click is in a table cell, we find its parent row's index to
-    // know which alarm item was clicked
-    const getIndexFromCell = (cell: HTMLElement) => {
-      const row = cell.parentElement
+    if (alarmId > 0) {
       const rows = table.current?.querySelectorAll('.table-body div[role="row"]')
-
-      let rowIndex = -1
-
-      if (rows && rows.length) {
-        for (let i = 0; i < rows.length; i++) {
-          // reference equality
-          if (rows[i] === row) {
-            rowIndex = i
-            break
-          }
-        }
-      }
-
-      return rowIndex
+      rowClicked(alarmId, e, rows ? Array.from(rows) : [] as Element[], series.current, true)
     }
 
-    const onTableClicked = (e: Event) => {
-      const rowIndex = getIndexFromCell(e.target as HTMLElement)
+    setMenu({ x: e.x, y: e.y })
+    setMenuOpen(() => true)
+  }
 
-      if (rowIndex >= 0) {
-        rowClicked(rowIndex, e as MouseEvent, false)
-      } else {
-        // user clicked on table background, clear all selections
-        const newIndexes = indexes.current.map(x => false)
-        setState({ indexes: newIndexes, lastClicked: -1 })
-      }
+  const addClassToTableBody = () => {
+    const headerGroup = table.current?.querySelector('div[role="rowgroup"] + div')
+    headerGroup?.classList.add('table-body')
+  }
+
+  // user made a regular click in the table
+  const onTableClicked = (e: Event) => {
+    const alarmId = getAlarmIdFromCell(e.target as HTMLElement, series.current)
+
+    if (alarmId > 0) {
+      // user clicked on a row, toggle selection
+      const rows = table.current?.querySelectorAll('.table-body div[role="row"]')
+      rowClicked(alarmId, e, rows ? Array.from(rows) : [] as Element[], series.current, false)
+    } else {
+      // user clicked on table background, clear all selections
+      const newIndexes = indexes.current.map(x => false)
+      setAlarmControlState({ selectedIndexes: newIndexes, selectedAlarmIds: new Set<number>(), lastClicked: -1, lastClickedAlarmId: alarmId })
+    }
+  }
+
+  // User made a context-click (usually a right-click)
+  const onTableContextMenu = (e: Event) => {
+    const alarmId = getAlarmIdFromCell(e.target as HTMLElement, series.current)
+
+    if (alarmId > 0) {
+      // user context-clicked a row, toggle selection for that row, then launch context menu
+      contextMenu(alarmId, e as MouseEvent)
+    } else if (selectedAlarmIds.current.size > 0) {
+      // user context-clicked but not in a row, but there are rows selected, so launch context menu
+      contextMenu(0, e as MouseEvent)
+    }
+  }
+
+  // apply click handlers to the table, but make sure they aren't applied to the Pagination component
+  useEffect(() => {
+    const currentTable = table.current
+    const tableWrapper = currentTable?.querySelector('div.alarm-table-wrapper')
+
+    tableWrapper?.addEventListener('click', onTableClicked)
+    tableWrapper?.addEventListener('contextmenu', onTableContextMenu)
+
+    return () => {
+      tableWrapper?.removeEventListener('click', onTableClicked)
+      tableWrapper?.removeEventListener('contextmenu', onTableContextMenu)
     }
 
-    const onTableContextMenu = (e: Event) => {
-      const rowIndex = getIndexFromCell(e.target as HTMLElement)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [table.current])
 
-      if (rowIndex >= 0) {
-        contextMenu(rowIndex, e as MouseEvent)
-      } else if (indexes.current.length > 0 && indexes.current.some(x => x === true)) {
-        contextMenu(-1, e as MouseEvent)
-      }
-    }
+  useEffect(() => {
+    addClassToTableBody()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [menu, menuOpen, rowClicked, series.current])
 
-    // apply click handlers to the table, but make sure they aren't applied to the Pagination component
-    useEffect(() => {
-        const currentTable = table.current
-        const tableWrapper = currentTable?.querySelector('div.alarm-table-wrapper')
-
-        tableWrapper?.addEventListener('click', onTableClicked)
-        tableWrapper?.addEventListener('contextmenu', onTableContextMenu)
-
-        return () => {
-          tableWrapper?.removeEventListener('click', onTableClicked)
-          tableWrapper?.removeEventListener('contextmenu', onTableContextMenu)
-        }
-
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [table.current])
-
-    useEffect(() => {
-        addClassToTableBody()
-    }, [menu, menuOpen, rowClicked, series])
-
-    return { table, menu, menuOpen, setMenu, setMenuOpen }
+  return { menu, menuOpen, setMenu, setMenuOpen }
 }
--- src/panels/alarm-table/hooks/useAlarmTableRowHighlighter.ts
+++ src/panels/alarm-table/hooks/useAlarmTableRowHighlighter.ts
@@ -1,29 +1,46 @@
-import { useEffect } from 'react'
+import { MutableRefObject, useEffect } from 'react'
+import { DataFrame } from '@grafana/data'
 import { AlarmTableControlState } from '../AlarmTableTypes'
+import { getAlarmIdsForRows } from '../AlarmTableHelper'
 
-export const useAlarmTableRowHighlighter = (state: AlarmTableControlState, table: React.MutableRefObject<HTMLDivElement | null>) => {
+export const useAlarmTableRowHighlighter = (state: AlarmTableControlState, table: React.MutableRefObject<HTMLDivElement | null>, frame: MutableRefObject<DataFrame>) => {
+  useEffect(() => {
+    const rows = table.current?.querySelectorAll('.table-body div[role="row"]')
 
-    useEffect(() => {
-        const rows = table.current?.querySelectorAll('.table-body div[role="row"]')
-        // if previous sibling is not clicked, or first item in entry, select-start
-        // if previous sibling is clicked, and next sibling is clicked, select-continue
-        // if previous sibling is clicked, and next sibling is not clicked, select-end
-        if (rows) {
-            rows.forEach((row, index) => {
-                row.classList.remove('select-start')
-                row.classList.remove('select-continue')
-                row.classList.remove('select-end')
-                if (!state.indexes[index - 1] && state.indexes[index]) {
-                    row.classList.add('select-start')
-                }
-                if (state.indexes[index - 1] && state.indexes[index] && state.indexes[index + 1]) {
-                    row.classList.add('select-continue')
-                }
-                if (state.indexes[index] && !state.indexes[index + 1]) {
-                    row.classList.add('select-end')
-                }
-            })
+    // if previous sibling is not clicked, or first item in entry, select-start
+    // if previous sibling is clicked, and next sibling is clicked, select-continue
+    // if previous sibling is clicked, and next sibling is not clicked, select-end
+    const alarmIdsForRows = getAlarmIdsForRows(rows ? Array.from(rows) : [] as Element[], frame.current)
+
+    if (rows) {
+      rows.forEach((row, index) => {
+        row.classList.remove('select-start')
+        row.classList.remove('select-continue')
+        row.classList.remove('select-end')
+
+        const previousRow = index > 0 ? rows[index - 1] : null
+        const nextRow = index < rows.length - 1 ? rows[index + 1] : null
+
+        const alarmId = alarmIdsForRows[index]
+        const previousAlarmId = previousRow ? alarmIdsForRows[index - 1] : -1
+        const nextAlarmId = nextRow ? alarmIdsForRows[index + 1] : -1
+
+        const thisRowSelected = state.selectedAlarmIds.has(alarmId)
+
+        if (thisRowSelected) {
+          const previousRowSelected = previousAlarmId > 0 && state.selectedAlarmIds.has(previousAlarmId)
+          const nextRowSelected = nextAlarmId > 0 && state.selectedAlarmIds.has(nextAlarmId)
+
+          if (!previousRowSelected) {
+            row.classList.add('select-start')
+          } else if (previousRowSelected && nextRowSelected) {
+            row.classList.add('select-continue')
+          } else if (!nextRowSelected) {
+            row.classList.add('select-end')
+          }
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    })
+      })
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  })
 }
